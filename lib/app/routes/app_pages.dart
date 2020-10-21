0d092af9189da79fc8a0e627e2c82140ea15a101
Marcos Vinicius <viniciusdejesus565@gmail.com>
2020-10-21 19:30:34 -0300

    Estrutura de Pastas

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,12 @@
+import 'package:barberapp/app/routes/app_routes.dart';
+import 'package:get/get.dart';
+
+class AppPages {
+  static final routes = [
+    GetPage(
+      name: Routes.INITIAL,
+      page: () => null,
+      binding: null,
+    ),
+  ];
+}
